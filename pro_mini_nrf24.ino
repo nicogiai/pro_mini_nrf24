8ebfca9d43cc52436d30f9175c09124ee1aa3e24
Nicolas Giai <nicogiai@gmail.com>
2021-05-23 21:22:00 -0300

    envia un payload de 2 floats 0:temp 1:humidity

--- pro_mini_nrf24.ino
+++ pro_mini_nrf24.ino
@@ -14,6 +14,34 @@
 #include "printf.h"
 #include "RF24.h"
 
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+#include "DHT.h"
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(7, 8); // using pin 7 for the CE pin, and pin 8 for the CSN pin
 
@@ -24,15 +52,18 @@ uint8_t address[][6] = {"1Node", "2Node"};
 
 // to use different addresses on a pair of radios, we need a variable to
 // uniquely identify which address this radio will use to transmit
-bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+bool radioNumber = 0; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
 
 // Used to control whether this node is sending or receiving
-bool role = false;  // true = TX role, false = RX role
+bool role = true;  // true = TX role, false = RX role
 
 // For this example, we'll be using a payload containing
 // a single float number that will be incremented
 // on every successful transmission
-float payload = 0.0;
+float payload[2] = {0.0,0.0};
+
+
+
 
 void setup() {
 
@@ -51,12 +82,12 @@ void setup() {
   Serial.println(F("RF24/examples/GettingStarted"));
 
   // To set the radioNumber via the Serial monitor on startup
-  Serial.println(F("Which radio is this? Enter '0' or '1'. Defaults to '0'"));
-  while (!Serial.available()) {
+  //Serial.println(F("Which radio is this? Enter '0' or '1'. Defaults to '0'"));
+  //while (!Serial.available()) {
     // wait for user input
-  }
-  char input = Serial.parseInt();
-  radioNumber = input == 1;
+  //}
+  //char input = Serial.parseInt();
+  //radioNumber = input == 1;
   Serial.print(F("radioNumber = "));
   Serial.println((int)radioNumber);
 
@@ -90,15 +121,25 @@ void setup() {
   // radio.printDetails();       // (smaller) function that prints raw register values
   // radio.printPrettyDetails(); // (larger) function that prints human readable data
 
+  dht.begin();
+
 } // setup
 
 void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  payload[1] = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  payload[0] = dht.readTemperature();
+  
   if (role) {
     // This device is a TX node
 
     unsigned long start_timer = micros();                    // start the timer
-    bool report = radio.write(&payload, sizeof(float));      // transmit & save the report
+    bool report = radio.write(&payload[0], sizeof(payload));      // transmit & save the report
     unsigned long end_timer = micros();                      // end the timer
 
     if (report) {
@@ -106,8 +147,8 @@ void loop() {
       Serial.print(F("Time to transmit = "));
       Serial.print(end_timer - start_timer);                 // print the timer result
       Serial.print(F(" us. Sent: "));
-      Serial.println(payload);                               // print payload sent
-      payload += 0.01;                                       // increment float payload
+      Serial.println(payload[0]);                               // print payload sent
+      Serial.println(payload[1]);                               // print payload sent
     } else {
       Serial.println(F("Transmission failed or timed out")); // payload was not delivered
     }
@@ -121,13 +162,13 @@ void loop() {
     uint8_t pipe;
     if (radio.available(&pipe)) {             // is there a payload? get the pipe number that recieved it
       uint8_t bytes = radio.getPayloadSize(); // get the size of the payload
-      radio.read(&payload, bytes);            // fetch payload from FIFO
+      radio.read(&payload[0], bytes);            // fetch payload from FIFO
       Serial.print(F("Received "));
       Serial.print(bytes);                    // print the size of the payload
       Serial.print(F(" bytes on pipe "));
       Serial.print(pipe);                     // print the pipe number
       Serial.print(F(": "));
-      Serial.println(payload);                // print the payload's value
+      Serial.println(payload[0]);                // print the payload's value
     }
   } // role
 
